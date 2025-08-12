5d085259d3fb67475071547e21febe594a359fdc
Henrique Lira <henriquelira.sp@gmail.com>
2025-08-12 12:50:22 -0300

    Reformulação de lógica | Estilização parcial

--- index.js
+++ index.js
@@ -1,64 +1,127 @@
-const form = document.getElementById("form")
+const addTechBtn = document.getElementById('addTechBtn');
+const devForm = document.getElementById('devForm');
+const developers = [];
+let inputCount = 0;
 
-form.addEventListener('submit', function(ev){
+addTechBtn.addEventListener('click', function(ev){
     ev.preventDefault()
+
+    inputCount++
+
+    const nameDev = document.getElementById("name").value
+    if(nameDev === "") {
+        alert("Por favor preencha seu nome")
+        return
+    } 
+
+    const ul = document.getElementById("stackInputs")
+    const li = document.createElement('li')        
+
+    //Criando input de tecnologias
+        const techLabel = document.createElement('label')
+        techLabel.textContent = "Tecnologia: "
+
+        const nameTech = document.createElement('input')
+        nameTech.type = "text"
+        nameTech.placeholder = "Digite a tecnologia..."
+        nameTech.id = `tech${inputCount}-name`
+        techLabel.htmlFor = nameTech.id
+
+    //Criando campo de experiencias
+    const radioName = `exp-${inputCount}`  
+
+    const labelExp1 = document.createElement('label')
+    labelExp1.textContent = "  0-2 anos "
+
+    const expTech1 = document.createElement('input')
+    expTech1.type = "radio"
+    expTech1.id = `experiencia1-${inputCount}`
+    expTech1.name = radioName
+    labelExp1.htmlFor = expTech1.id
+
+    const labelExp2 = document.createElement('label')
+    labelExp2.textContent = " 3-4 anos "
+
+    const expTech2 = document.createElement('input')
+    expTech2.type = "radio"
+    expTech2.id = `experiencia2-${inputCount}`
+    expTech2.name = radioName
+    labelExp2.htmlFor = expTech2.id
+
+    const labelExp3 = document.createElement('label')
+    labelExp3.textContent = " +5 anos  "
+
+    const expTech3 = document.createElement('input')
+    expTech3.type = "radio"
+    expTech3.id = `experiencia3-${inputCount}`
+    expTech3.name = radioName
+    labelExp3.htmlFor = expTech3.id
+
+    //Criando botão remover tecnologia
+        const removeTechBtn = document.createElement('button')
+        removeTechBtn.type = "button"
+        removeTechBtn.textContent = " Excluir "
+        removeTechBtn.addEventListener('click', function(){
+            li.remove()
+        })
+        
+        li.append(techLabel, nameTech, 
+            expTech1, labelExp1 ,
+            expTech2, labelExp2,
+            expTech3, labelExp3,
+            removeTechBtn)
+        ul.appendChild(li)
+
 })
 
-const initButton = document.getElementById("initButton")
-    initButton.addEventListener('click', function(){
+const registerDev = document.getElementById("check")
+registerDev.addEventListener('click', function(ev){
+    ev.preventDefault()
 
-        const div = document.getElementById("techList")
-        const divTech = document.createElement("div")
+    const nameDev = document.getElementById("name").value
+    if(nameDev === ""){
+        alert("Por favor preencha seu nome.")
+    }
 
-        const inputTech = document.createElement("input")
-        inputTech.type = "text"
-        inputTech.className = "input_techOpitions"
-        inputTech.placeholder = "Digite sua tecnologia..."
-        inputTech.required = true //Torna o campo obrigatório
-        inputTech.name = "technology"
-        inputTech.id = "technology"
-        inputTech.autocomplete = "off" //Desativa o autocomplete do navegador
+    const stackInputs = document.getElementById('stackInputs')
+    if(stackInputs.length === 0) {
+        alert("Adicione pelo menos uma tecnologia.")
+    }
 
+    const devList = []
 
-        //Criação de label e input
-        const labelExp1 = document.createElement("label")
-        labelExp1.htmlFor = "experience1" 
-        labelExp1.textContent = "0-2 anos " //Adiciona texto na label
+    const techItens = stackInputs.querySelectorAll('li')
+    techItens.forEach(item => {
+        const nameTech = item.querySelector('input[type="text"]').value
+        if(nameTech === ""){
+            alert("Adicione pelo menos uma tecnologia.")
+            return
+        }
+        const selectedExp = item.querySelector('input[type="radio"]:checked')
+        if(!selectedExp){
+            alert("Informe seu temopo de experiência.")
+            return
+        }
 
-        const inputExperience1 = document.createElement("input")
-        inputExperience1.type = "radio"
-        inputExperience1.name = "experience"
-        inputExperience1.id = "experience1"
-
-        const labelExp2 = document.createElement("label")
-        labelExp2.htmlFor = "experience2"
-        labelExp2.textContent = "3-4 anos "
-
-        const inputExperience2 = document.createElement("input")
-        inputExperience2.type = "radio"
-        inputExperience2.name = "experience"
-        inputExperience2.id = "experience2"
-
-        const labelExp3 = document.createElement("label")
-        labelExp3.htmlFor = "experience3"
-        labelExp3.textContent = "+5 anos "
-
-        const inputExperience3 = document.createElement("input")
-        inputExperience3.type = "radio"
-        inputExperience3.name = "experience"
-        inputExperience3.id = "experience3"
-
-        //Criação do botão de remover tecnologia
-        const removeButton = document.createElement("button")
-        removeButton.type = "button"
-        removeButton.textContent = "Remover Tecnologia"
-        removeButton.className = "removeButton"
-        removeButton.addEventListener('click', function(){
-        div.removeChild(divTech)    
+        const expTime = selectedExp.value
+        
+        devList.push({
+            tecnologia: nameTech,
+            experiencia: expTime
         })
 
-        divTech.append(inputTech, inputExperience1, labelExp1, inputExperience2, labelExp2, inputExperience3, labelExp3, removeButton)
-        div.appendChild(divTech)
+        developers.push({
+            nome: nameDev,
+            tecnologias: devList
+        })
 
-        
-    })
+
+        console.log(developers)
+    });
+    
+})
+
+
+
+
+
